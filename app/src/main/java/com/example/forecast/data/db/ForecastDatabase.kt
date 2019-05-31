0985a4dc84059ea995f71fbd76b471a02f95805c
Kun <kun.register@gmail.com>
2019-05-31 20:05:28 +0100

    Add more comments explaining the code

--- app/src/main/java/com/example/forecast/data/db/ForecastDatabase.kt
+++ app/src/main/java/com/example/forecast/data/db/ForecastDatabase.kt
@@ -6,25 +6,62 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.forecast.data.db.entity.CurrentWeatherEntry
 
+
+// Set tables
 @Database(
     entities = [CurrentWeatherEntry::class],
     version = 1
 )
-abstract class ForecastDatabase : RoomDatabase () {
-    abstract fun currentWeatherDao() : CurrentWeatherDao
+abstract class ForecastDatabase : RoomDatabase() {
+    // Dao to be used by upper layer (Repository)
+    abstract fun currentWeatherDao(): CurrentWeatherDao
 
-    companion object{
-        @Volatile private var instance: ForecastDatabase? = null
-        private val LOCK = Any()
+    companion object {
+        private var instance: ForecastDatabase? = null
 
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+        // When a static class is loaded, a companion object is also instantiated.
+        // The name of the object is "ForecastDatabase", and its type is ForecastDatabase.Companion
+        // What we do here is we overload the invoke operator for ForecastDatabase object.
+        // Lastly, we encapsulate the initialisation of the instance in the ForecastDatabase invocation.
+        operator fun invoke(context: Context) = instance ?: synchronized(this){
             instance ?: buildDatabase(context).also { instance = it }
         }
 
-        private fun buildDatabase(context: Context) = Room.databaseBuilder(
-            context.applicationContext,
-            ForecastDatabase::class.java,
-            "forecast.db"
-        ).build()
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context,
+                ForecastDatabase::class.java,
+                "forecast.db"
+            ).build()
+
     }
 }
+
+
+//@Database(
+//    entities = [CurrentWeatherEntry::class],
+//    version = 1
+//)
+//abstract class ForecastDatabase : RoomDatabase() {
+//    abstract fun currentWeatherDao(): CurrentWeatherDao
+//
+//    companion object {
+//        // Apply volatile to make sure this variable keeps its changes among all threads.
+//        @Volatile
+//        private var instance: ForecastDatabase? = null
+//        // A lock to be used in synchronization block
+//        private val LOCK = Any()
+//
+//        // Initialisation is executed in synchronization block.
+//        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+//            instance ?: buildDatabase(context).also { instance = it }
+//        }
+//
+//        private fun buildDatabase(context: Context) =
+//            Room.databaseBuilder(
+//                context,
+//                ForecastDatabase::class.java,
+//                "forecast.db"
+//            ).build()
+//    }
+//}
--- app/src/main/java/com/example/forecast/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/example/forecast/ui/weather/current/CurrentWeatherViewModel.kt
@@ -7,6 +7,7 @@ import com.example.forecast.internal.lazyDeferred
 class CurrentWeatherViewModel(
     private val forecastRepository: ForecastRepository
 ) : ViewModel() {
+    // observable property. Knows nothing about the view. LiveData is the observable property.
     val currentWeatherLiveData by lazyDeferred {
         forecastRepository.getCurrentWeather(true)
     }
